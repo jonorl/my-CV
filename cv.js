218314721ba24ae27377e7749c7cfabf270d1428
jonorl <39122230+jonorl@users.noreply.github.com>
2024-08-05 17:33:56 +0300

    Add files via upload
    Uploading my CV

---
+++ cv.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    
+    toggleButton.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+});
